a72dd78901bf565f173449dea3daedde7ba29766
Shark-PT <jccreis@hotmail.com>
2023-11-30 13:54:20 +0000

    Função Search completa. Mais error handlings

--- main.py
+++ main.py
@@ -63,6 +63,24 @@ def save():
             email_entry.delete(0, END)
             password_input.delete(0, END)
 
+# ---------------------------FIND PASSWORD ------------------------------ #
+
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found.")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -75,30 +93,29 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
+# Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
-
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
-
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
+# Entries
+website_entry = Entry(width=20)
+website_entry.grid(row=1, column=1, sticky="EW")
 website_entry.focus()
-
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
 email_entry.insert(0, "username@e-mail.com")
-
-password_input = Entry(width=21)
+password_input = Entry(width=20)
 password_input.grid(row=3, column=1, sticky="EW")
 
-
+# Buttons
+search_button = Button(text="Search", command=find_password)
+search_button.grid(row=1, column=2, sticky="EW")
 password_button = Button(text="Generate Password", command=generate_password)
 password_button.grid(row=3, column=2, sticky="EW")
-
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
 
